c5fcdf1bc8649af9ae42e21ee6f8fa088f6bed2a
Tarun Soni <soni.tarun26@gmail.com>
2020-12-01 23:59:41 +0530

    initial release

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import profile_links from "./links";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        <div className="container">
+          <h4>Hi 👋, I'm Tarun. Know more about me ...</h4>
+          {profile_links.map((m, index) => (
+            <a
+              href={m.link}
+              target="_blank"
+              rel="noopener noreferrer"
+              key={index}
+            >
+              <div className="item">
+                <img className="item-img" src={m.img} alt="icon" />
+                {m.name}
+                <i className="fas fa-external-link-alt"></i>
+              </div>
+            </a>
+          ))}
+        </div>
+      </div>
+      <footer>
+        <div style={{ display: "none" }}>
+          Icons made by
+          <a href="https://www.flaticon.com/authors/freepik" title="Freepik">
+            Freepik
+          </a>
+          from
+          <a href="https://www.flaticon.com/" title="Flaticon">
+            www.flaticon.com
+          </a>
+        </div>
+      </footer>
+    </>
   );
-}
+};
 
 export default App;
---
+++ src/links.js
@@ -0,0 +1,30 @@
+import linkedin from "./assets/linkedin.png";
+import github from "./assets/github.png";
+import medium from "./assets/medium.png";
+import resume from "./assets/resume.png";
+
+const profile_links = [
+  {
+    name: "  LinkedIn",
+    link: "https://www.linkedin.com/in/tarun-b-soni/",
+    img: linkedin,
+  },
+  {
+    name: "Github",
+    link: "https://github.com/tarun-soni",
+    img: github,
+  },
+
+  {
+    name: "Resume",
+    link:
+      "https://drive.google.com/file/d/1VRANWEcchUKEyA6h1Vb33MkRyhR48AFS/view?usp=sharing",
+    img: resume,
+  },
+  {
+    name: "Medium",
+    link: "https://medium.com/@soni.tarun26",
+    img: medium,
+  },
+];
+export default profile_links;
